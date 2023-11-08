4b39fa641baf774a08a27bae24cbbd67a2ca7656
chunge <1362598656@qq.com>
2023-11-08 17:35:44 +0800

    refactor: :recycle: 优化icon的图片

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vitepress';
 import {processData} from '../../vitepress-blogs-theme/config/index.js';
 // https://vitepress.dev/reference/site-config
+const base = "/vitepress-blogs/";
 export default defineConfig({
-  base: "/vitepress-blogs/",
+  base,
   cleanUrls: true,
   title: "春哥的博客",
   description: "行到水穷处，坐看云起时",
@@ -15,8 +16,8 @@ export default defineConfig({
     ['meta', { name: 'apple-mobile-web-app-title', content: 'name' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'default' }],
 
-    ['link', { rel: 'shortcut icon', href: '/favicon.ico' }],
-    ['link', { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    ['link', { rel: 'shortcut icon', href: `${base}cat-with-wry-smile.svg` }],
+    ['link', { rel: 'icon', type: 'image/x-icon', href: `${base}cat-with-wry-smile.svg` }],
 
   ],
   themeConfig: {
